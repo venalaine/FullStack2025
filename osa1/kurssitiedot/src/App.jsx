e008056536855073ade3ca8ef3f83bddb27ded10
Erno <ernovenalainen@hotmail.com>
2025-07-31 13:19:35 +0300

    test

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,65 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+
+  const Header = ( {course}) =>  {
+    return (
+      <>
+        <h1>{course.name}</h1>
+      </>
+    )
+  }
+
+  const Content = ({course}) => {
+    return (
+      <>
+        <Part part={course.parts[0]}/>
+        <Part part={course.parts[1]}/>
+        <Part part={course.parts[2]}/>
+      </>
+    )
+  }
+
+  const Part = ( {part}) => {
+    return (
+      <>
+        <p>{part.name} {part.exercises}</p>
+      </>
+    )
+  }
+  
+  const Total = ({course}) => {
+    const total = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+    return (
+      <>
+        <p>Number of exercises {total}</p> 
+      </>
+    )
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total course={course}/>
+    </div>
+  )
+}
+
+export default App
