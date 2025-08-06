b6e7a288dc42b1204880df18a18a450687793a37
Erno <ernovenalainen@hotmail.com>
2025-08-06 08:41:26 +0300

    part2  kurssitiedot step6 done

---
+++ osa2/kurssitiedot rev2/src/App.jsx
@@ -0,0 +1,72 @@
+const Header = ({ name }) => {
+  return (
+    <>
+      <h1>{name}</h1>
+    </>
+  )
+}
+
+const Course = ({ course }) => {
+
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+
+}
+
+const Part = ({ part }) => {
+  return (
+    <>
+      <p key={part.id}>
+        {part.name}
+        {' '}
+        {part.exercises}
+      </p>
+    </>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
